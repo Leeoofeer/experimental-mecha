dc72c7e54ef45c13a94ebd5ec4f0ad8664cf8796
leeoofeerbf <leeoofeerbf@gmail.com>
2024-04-05 01:17:25 +0200

    Build Polish

--- TDAH el juego/Assets/Scripts/GameManager.cs
+++ TDAH el juego/Assets/Scripts/GameManager.cs
@@ -140,4 +140,9 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W)) { PlayerWalked(); }
     }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
